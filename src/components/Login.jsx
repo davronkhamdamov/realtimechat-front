398c6de43ab707ed07a9c8e165b41ecef8413e98
davronkhamdamov <xamdamovdavron6@gmail.com>
2023-08-02 09:55:39 +0500

    Login page complated

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 import {
   Button,
   FormControl,
@@ -7,20 +8,68 @@ import {
   InputGroup,
   InputRightElement,
   VStack,
+  useToast,
 } from "@chakra-ui/react";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [show, setShow] = useState(false);
+  const [laoding, setloading] = useState(false);
+  const toast = useToast();
 
-  const postImg = (pic) => {};
-  const submitHandler = () => {};
+  const submitHandler = async () => {
+    setloading(false);
+    if (!email || !password) {
+      toast({
+        title: "Please fill all the Fields",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setloading(false);
+      return;
+    }
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+      const { data } = await axios.post(
+        "/api/user/login",
+        { email, password },
+        config
+      );
+      toast({
+        title: "Login Successful",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      setloading(false);
+      window.location("chat");
+    } catch (error) {
+      setloading(false);
+      toast({
+        title: "Error Occured!",
+        description: error.response.data.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+    }
+  };
   return (
     <VStack spacing="5px" color="black">
       <FormControl id="email" isRequired>
         <FormLabel>Email</FormLabel>
         <Input
+          value={email}
           placeholder="Enter your email"
           onChange={(e) => setEmail(e.target.value)}
         />
@@ -29,6 +78,7 @@ const Login = () => {
         <FormLabel>Password</FormLabel>
         <InputGroup>
           <Input
+            value={password}
             type={show ? "text" : "password"}
             placeholder="Enter your password"
             onChange={(e) => setPassword(e.target.value)}
@@ -41,12 +91,24 @@ const Login = () => {
         </InputGroup>
       </FormControl>
       <Button
+        isLoading={laoding}
         colorScheme="blue"
         width="100%"
         style={{ marginTop: 15 }}
         onClick={() => submitHandler()}
       >
         Login
+      </Button>
+      <Button
+        variant={"solid"}
+        colorScheme="red"
+        width="100%"
+        onClick={() => {
+          setEmail("guest@example.com");
+          setPassword("123456");
+        }}
+      >
+        Get Guest User Credentials
       </Button>
     </VStack>
   );
