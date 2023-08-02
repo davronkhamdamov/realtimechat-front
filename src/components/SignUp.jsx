6f7bfcb390fcd311fcc41a261ddc5fd3a710288d
davronkhamdamov <xamdamovdavron6@gmail.com>
2023-08-02 09:41:32 +0500

    sign up complated

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -6,8 +6,11 @@ import {
   InputGroup,
   InputRightElement,
   VStack,
+  useToast,
 } from "@chakra-ui/react";
 import { useState } from "react";
+import axios from "axios";
+
 const SignUp = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -15,9 +18,108 @@ const SignUp = () => {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [pic, setPic] = useState("");
   const [show, setShow] = useState(false);
+  const [loading, setloading] = useState(false);
+  const toast = useToast();
 
-  const postImg = (pic) => {};
-  const submitHandler = () => {};
+  function postImg(formData) {
+    setloading(true);
+    if (formData === undefined) {
+      toast({
+        title: "Please Select an Image!",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      return;
+    }
+    if (["image/jpeg", "image/png"].includes(formData.type)) {
+      const data = new FormData();
+      data.append("file", formData);
+      data.append("upload_preset", "youtube");
+      fetch("https://api.cloudinary.com/v1_1/didddubfm/image/upload", {
+        method: "POST",
+        body: formData,
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setPic(data.url);
+          setloading(false);
+        })
+        .catch((err) => {
+          console.log(err);
+          setloading(false);
+        });
+    } else {
+      toast({
+        title: "Please Select an Image!",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+    }
+  }
+  const submitHandler = async () => {
+    setloading(true);
+    if (!name || !email || !password || !confirmPassword) {
+      toast({
+        title: "Please Fill all Fields!",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setloading(false);
+      return;
+    }
+    if (password !== confirmPassword) {
+      toast({
+        title: "Password do not match",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      return;
+    }
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+      const { data } = await axios.post(
+        "/api/user/register",
+        {
+          name,
+          email,
+          password,
+          pic,
+        },
+        config
+      );
+      toast({
+        title: "Registration Successfully",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      setloading(false);
+    } catch (error) {
+      setloading(false);
+      toast({
+        title: "Error Occured!",
+        description: error.response.data.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+    }
+  };
   return (
     <VStack spacing="5px" color="black">
       <FormControl id="first-name" isRequired>
@@ -55,7 +157,7 @@ const SignUp = () => {
           <Input
             type={show ? "text" : "password"}
             placeholder="Enter your confirm password"
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={(e) => setConfirmPassword(e.target.value)}
           />
           <InputRightElement width="4.5rem">
             <Button h="1.75rem" size="sm" onClick={() => setShow(!show)}>
@@ -69,11 +171,12 @@ const SignUp = () => {
         <Input
           type="file"
           p={1.5}
-          accept="image/*"
+          accept="image/png, image/jpeg"
           onChange={(e) => postImg(e.target.files[0])}
         />
       </FormControl>
       <Button
+        isLoading={loading}
         colorScheme="blue"
         width="100%"
         style={{ marginTop: 15 }}
